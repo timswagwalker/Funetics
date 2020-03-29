799f88a27181cae8d7a05229201a590447805770
Tanay Nistala <nistalatanay@gmail.com>
2020-03-29 16:58:26 +0530

    Initial Commit

---
+++ Funetics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Funetics
+//
+//  Created by Tanay Nistala on 3/29/20.
+//  Copyright © 2020 Tanay Nistala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
